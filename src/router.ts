f2d5c9b0858bbf94e3bd63af1b4aed09c66bc62e
Cesar Trujillo <catgx55@gmail.com>
2024-08-21 22:54:05 -0500

    crear producto y validacion

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from "express"
+import { createProduct } from "./handlers/product"
+import { body } from "express-validator"
 
 const router = Router()
 
@@ -7,9 +9,18 @@ router.get('/', (req, res) => {
     res.json('Desde GET')
 })
 
-router.post('/', (req, res) => {
-    res.json('Desde POST')
-})
+router.post('/', 
+    // validación
+    body('name')
+        .notEmpty().withMessage('El nombre del producto no puede estar vacio'),
+    body('price')
+        .isNumeric().withMessage('Valor no válido')
+        .notEmpty().withMessage('El price del producto no puede estar vacio')
+        .custom(value => value > 0).withMessage('Precio no valido'),
+
+    createProduct
+
+)
 
 router.put('/', (req, res) => {
     res.json('Desde PUT')
